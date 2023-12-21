253a7c4649f9048da203d9e632e6bdd08564bffe
Douwe den Blanken <V0XNIHILI@users.noreply.github.com>
2023-12-21 19:38:48 +0100

    Revert "Make linting and formatting actions work correctly (#4)"
    This reverts commit cd84f6bdda5f2f5acdfa6bdf732b84675eda7230.

--- scripts/format.sh
+++
@@ -1,7 +0,0 @@
-files=$(find "../" -type f -name "*.v")
-files+=$(find "../" -type f -name "*.sv")
-
-for file in $files; do
-    echo "Formatting $file"
-    verible-verilog-format --flagfile=../.verible-format-flags $file
-done
