537189b389bd1680eb8a9feb464ca8670bb50004
Douwe den Blanken <douwedb@gmail.com>
2023-12-25 13:08:27 +0100

    Update leading_one_detector module to use 'value' instead of 'in' variable and remove latches and fix indexing of pre-pended zeros

--- src/leading_one_detector.v
+++ src/leading_one_detector.v
@@ -4,20 +4,30 @@
 module leading_one_detector #(
     parameter int WIDTH = 8
 ) (
-    input [WIDTH-1:0] in,
+    input [WIDTH-1:0] value,
     output reg [$clog2(WIDTH)-1:0] position,
     output has_leading_one
 );
 
-    assign has_leading_one = in != 0;
+    assign has_leading_one = value != 0;
+
+    // Can also store boolean per bit, and then take the first true value in another loop
 
     genvar i;
     for (i = WIDTH - 1; i >= 0; i = i - 1) begin : gen_leading_one_detector_for_loop
         always_comb begin
             if (has_leading_one) begin
-                if (in[i] == 1'b1 && (i == WIDTH - 1 ? 1'b1 : in[WIDTH-1:i+1] == 0)) begin
+                if (value[i] == 1'b1 && (i == WIDTH - 1 ? 1'b1 : value[WIDTH-1:i] == 0)) begin
+                    $display("Found leading one at position %d.", i);
+
                     position = i;
+                end else begin
+                    position = {$clog2(WIDTH){1'bz}};
                 end
+            end else begin
+                $display("No leading one found.");
+
+                position = {$clog2(WIDTH){1'bx}};
             end
         end
     end
