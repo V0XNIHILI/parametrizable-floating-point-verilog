492e6ccb6e380309849b3fdfec4ab8e34010962f
Douwe den Blanken <douwedb@gmail.com>
2023-12-28 12:45:14 +0100

    Rewrite leading one detector with break statement

--- src/leading_one_detector.v
+++ src/leading_one_detector.v
@@ -6,28 +6,22 @@ module leading_one_detector #(
 ) (
     input [WIDTH-1:0] value,
     output reg [$clog2(WIDTH)-1:0] position,
-    output has_leading_one
+    output reg has_leading_one
 );
 
-    assign has_leading_one = value != 0;
+    integer i;
 
-    // Can also store boolean per bit, and then take the first true value in another loop
+    always_comb begin
+        position = {$clog2(WIDTH) {1'bx}};
+        has_leading_one = value != 0;
 
-    genvar i;
-    for (i = WIDTH - 1; i >= 0; i = i - 1) begin : gen_leading_one_detector_for_loop
-        always_comb begin
-            if (has_leading_one) begin
-                if (value[i] == 1'b1 && (i == WIDTH - 1 ? 1'b1 : value[WIDTH-1:i] == 0)) begin
-                    $display("Found leading one at position %d.", i);
-
-                    position = i;
-                end else begin
-                    position = {$clog2(WIDTH){1'bz}};
-                end
-            end else begin
-                $display("No leading one found.");
-
-                position = {$clog2(WIDTH){1'bx}};
+        for (i = WIDTH - 1; i >= 0; i = i - 1) begin
+            if (value[i] == 1'b1) begin
+                // Index selection to avoid the error:
+                // "Operator ASSIGN expects 5 bits on the Assign RHS, but Assign RHS's VARREF 'i' generates 32 bits."
+                // from Verilator.
+                position = i[$clog2(WIDTH)-1:0];
+                break;
             end
         end
     end
